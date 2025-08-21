c667c07ba133a28d04db813d309e73fdd7ed9ff8
Emmanuel <demey.emmanuel@gmail.com>
2025-08-21 13:40:50 +0100

    feat: migrate server code

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,46 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import axios from "axios";
+import bodyParser from "body-parser";
 
 const api = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const REDIRECT_URI = "https://lktoken.netlify.app/api/callback";
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
 
+app.get("/auth", (req, res) => {
+  const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=r_organization_social`;
+  res.redirect(authUrl);
+});
+
+app.get("/callback", async (req, res) => {
+  const authCode = req.query.code;
+
+  try {
+    const tokenResponse = await axios.post(
+      "https://www.linkedin.com/oauth/v2/accessToken",
+      null,
+      {
+        params: {
+          grant_type: "authorization_code",
+          code: authCode,
+          redirect_uri: REDIRECT_URI,
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+        },
+      }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+    res.send(`Token d'accès obtenu : ${accessToken}`);
+  } catch (error) {
+    console.error("Erreur lors de l'obtention du token :", error);
+    res.status(500).send("Erreur d'authentification.");
+  }
+});
 api.use("/api/", router);
 
 export const handler = serverless(api);
